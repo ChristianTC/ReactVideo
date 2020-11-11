5ace3286f2cc3c58da98b11853065ccf69bd14cd
ChristianTC <christiantambo15@gmail.com>
2020-11-11 00:39:21 -0400

    Header structure and styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld.jsx'
+import Header from './components/Header.jsx'
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+import App from '../src/containers/App'
+
+ReactDOM.render(<App />, document.getElementById('app'));
