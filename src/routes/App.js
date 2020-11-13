4cc7d694a14b84a9a1597a1c7d36501df9bf87ce
ChristianTC <christiantambo15@gmail.com>
2020-11-12 20:16:37 -0400

    Container Register added with styles and Routes

--- src/routes/App.js
+++ src/routes/App.js
@@ -4,11 +4,14 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'; 
 import Home from '../containers/Home';
 import Login from '../containers/Login';
+import Register from '../containers/Register';
+
 // funcion que va a exponer Browser Router
 const App = () => (
     <BrowserRouter>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
     </BrowserRouter>
 );
 
