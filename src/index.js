3d4e8617c7a997fc5ee2f8535e8cd3ef7d1b05f2
ChristianTC <christiantambo15@gmail.com>
2020-11-13 13:35:39 -0400

    Redux installed, directories actions and reducers with files index.js created; and Provider imported in index.js from src

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import Header from './components/Header.jsx'
 
 import App from './routes/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, 
+    
+    <Provider>
+        <App />
+    </Provider>,
+    
+    document.getElementById('app')
+);
